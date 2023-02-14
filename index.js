ee8588ffff359dd53617d78f6dd46e426b77d416
callmejkode <callmejkode@gmail.com>
2023-02-14 17:14:20 +0530

    basic nav and html

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const http = require("http");
+const {readFileSync} = require('fs');
+
+const homePage = readFileSync('./Header/index.html');
+
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  res.writeHead(200, { "Content-Type": "text/html" });
   if (url === "/") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("hello world!");
+    
+    res.write(homePage);
     res.end();
   } else if (url === "/about") {
-    res.writeHead(200, { "Content-Type": "text/html" });
+    
     res.write("hello about!");
     res.end();
   } else if (url === "/contact") {
-    res.writeHead(200, { "Content-Type": "text/html" });
+    
     res.write("hello contact!");
     res.end();
   } else {
