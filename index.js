3f2cb2431ecef1e3b7167cb2925b87e37fd8e658
callmejkode <callmejkode@gmail.com>
2023-02-15 22:30:19 +0530

    home page edit

--- index.js
+++ index.js
@@ -1,35 +1,47 @@
-const http = require("http");
-const {readFileSync} = require('fs');
+const express = require('express');
+const path = require('path');
 
-const homePage = readFileSync('./Header/index.html');
+const app = express();
+const port = process.env.PORT || 5000;
 
-
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  res.writeHead(200, { "Content-Type": "text/html" });
-  if (url === "/") {
-    
-    res.write(homePage);
-    res.end();
-  } else if (url === "/about") {
-    
-    res.write("hello about!");
-    res.end();
-  } else if (url === "/contact") {
-    
-    res.write("hello contact!");
-    res.end();
-  } else {
-    res.writeHead(404, { "Content-Type": "text/html" });
-    res.write("Page not found");
-    res.end();
-  }
+// sendFile will go here
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
-const port = 5000;
-server.listen(port, (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(`Server listening on ${port}`);
-  }
-});
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
+
+
+
+
+// const server = http.createServer((req, res) => {
+//   const url = req.url;
+//   res.writeHead(200, { "Content-Type": "text/html" });
+//   if (url === "/") {
+    
+//     res.write(homePage);
+//     res.end();
+//   } else if (url === "/about") {
+    
+//     res.write("hello about!");
+//     res.end();
+//   } else if (url === "/contact") {
+    
+//     res.write("hello contact!");
+//     res.end();
+//   } else {
+//     res.writeHead(404, { "Content-Type": "text/html" });
+//     res.write("Page not found");
+//     res.end();
+//   }
+// });
+
+// const port = 5000;
+// app.listen(port, (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(`Server listening on ${port}`);
+//   }
+// });
